8ca4d21c6c83125b7212a14218ecdadc97f36395
JoeBrosnihan <joebrosnihan@gmail.com>
2016-02-20 05:39:01 -0500

    Compile with FireBase

--- app/src/main/java/com/yoonshikhong/sticktogether/MapsActivity.java
+++ app/src/main/java/com/yoonshikhong/sticktogether/MapsActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
+import com.firebase.client.Firebase;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
@@ -52,6 +53,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+	    Firebase.setAndroidContext(this);
+
+	    Firebase myFirebaseRef = new Firebase("https://sweltering-inferno-8609.firebaseio.com/");
+		myFirebaseRef.child("newmessage").setValue("I have data!!!");
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
